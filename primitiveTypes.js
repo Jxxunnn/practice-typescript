839a2795384f5913c8b5b72871ec3aa960fafd37
Jxxunnn <roof2586@naver.com>
2023-01-11 13:41:55 +0900

    study: 타입스크립트 primitive type

---
+++ primitiveTypes.js
@@ -0,0 +1,5 @@
+var 이름 = "kim";
+var 나이 = 20;
+var 결혼했니 = false;
+var 회원들 = ["kim", "park"];
+var 내정보 = { age: 20 };
---
+++ primitiveTypes.ts
@@ -0,0 +1,6 @@
+let 이름 :string = "kim";
+let 나이 :number = 20;
+let 결혼했니 :boolean =false;
+
+let 회원들 :string[] = ["kim", "park"];
+let 내정보 :{age : number} = {age : 20};
